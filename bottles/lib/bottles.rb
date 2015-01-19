847189a1483fa94ea793bd36ab02edc2c55c49fe
Lizz Armstrong + Enric Ribas <dev+la+er@influitive.com>
2015-01-19 15:01:53 -0500

    wip

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -2,5 +2,25 @@
 puts instructions(__FILE__)
 
 class Bottles
+  def verse(num)
+    case(num)
+    when(2)
+      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+    when(1)
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+    when(0)
+      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\nTake one down and pass it around, #{num-1} bottles of beer on the wall.\n"
+    end
+  end
 
+  def verses(start, stop)
+    v = []
+    start.downto(stop) do |i|
+      v << verse(i)
+    end
+
+    return v.join("\n")
+  end
 end
